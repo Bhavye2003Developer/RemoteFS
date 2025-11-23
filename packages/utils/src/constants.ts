f51b7dddcf18649f73e75a3c7ac7bf87cd947006
cybergod <bhavyedevelopment2003@gmail.com>
2025-11-23 15:46:07 +0530

    changed private IP to template IP

--- packages/utils/src/constants.ts
+++ packages/utils/src/constants.ts
@@ -1,4 +1,4 @@
 // import fetchIP from "./fetchIP";
-export const SYSTEM_IP = "192.168.1.40";
+export const SYSTEM_IP = "192.168.1.x"; // Replace with your IP
 export const SERVER_PORT = 3125;
-export const CLIENT_PORT = 5173;
+// export const CLIENT_PORT = 5173;
